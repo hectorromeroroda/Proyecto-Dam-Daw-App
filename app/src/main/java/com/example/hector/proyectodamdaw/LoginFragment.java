6661300c88c103ab554d20612b6c7e30699085e5
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-03 17:56:25 +0200

    Diseño LoginActivity practicamente acabado
    Falta dar funcionalidades al boton y a los Links, poner logo de la App, decidir itemps navigation drawer

--- app/src/main/java/com/example/hector/proyectodamdaw/LoginFragment.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/LoginFragment.java
@@ -1,16 +1,27 @@
 package com.example.hector.proyectodamdaw;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Hector on 31-Mar-18.
  */
 
 public class LoginFragment extends Fragment{
+
+    TextView forgotPassw;
+    TextView singUpLogin;
+    EditText userLogin;
+    EditText  passwLogin;
+    Button acceptLogin;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -21,12 +32,42 @@ public class LoginFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.login_fragment, container, false);
 
+        forgotPassw =(TextView) view.findViewById(R.id.txvForgotPassw);
+        singUpLogin =(TextView) view.findViewById(R.id.txvSingUpLogin);
+        userLogin = (EditText)view.findViewById(R.id.edtUserLogin);
+        passwLogin = (EditText)view.findViewById(R.id.edtPasswLogin);
+        acceptLogin = (Button) view.findViewById(R.id.btnAcceptLogin);
+
         return view;
     }
 
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
 
+        forgotPassw.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+               //AQUI ACCION A REALIZAR AL PULSAR "FORGOT PASSWORD"
+            }
+
+        });
+
+        singUpLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                //AQUI ACCION A REALIZAR AL PULSAR "SING-UP LOGIN"
+
+            }
+
+        });
+
+        acceptLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //AQUI ACCION A REALIZAR AL PULSAR boton "AcceptLogin"
+            }
+        });
 
     }
 }
