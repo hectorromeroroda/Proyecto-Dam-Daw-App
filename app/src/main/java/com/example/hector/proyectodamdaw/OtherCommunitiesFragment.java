1bd71574ef6293a80f99560af68380b48115c3f5
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-05 22:21:14 +0200

    Diseño yourCommunitiesFragment terminado
    El diseño esta acabado, pero falta hacer la Row de una comunidad y el adaptador para cargar los reciclerview's

--- app/src/main/java/com/example/hector/proyectodamdaw/OtherCommunitiesFragment.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/OtherCommunitiesFragment.java
@@ -7,28 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
- * Created by Hector on 03-Apr-18.
+ * Created by Hector on 05/04/2018.
  */
 
-public class YourCommunitiesFragment extends Fragment{
+public class OtherCommunitiesFragment extends Fragment{
 
-    protected RecyclerView.LayoutManager layoutManager;
-    protected RecyclerView recyclerViewYourCommunities;
-    protected RecyclerView recyclerViewYourInvitations;
-
-    public YourCommunitiesFragment() {
+    public OtherCommunitiesFragment() {
         // Required empty public constructor
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.your_communities_fragment, container, false);
+        View view = inflater.inflate(R.layout.other_communities_fragment, container, false);
 
-        recyclerViewYourCommunities = (RecyclerView) view.findViewById(R.id.rcvYourCommunities);
-        recyclerViewYourInvitations = (RecyclerView) view.findViewById(R.id.rcvYourinvitations);
+
 
         return view;
     }
