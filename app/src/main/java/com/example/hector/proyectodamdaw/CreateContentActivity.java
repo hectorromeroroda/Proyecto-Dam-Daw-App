e38ba948869df72ceb09fc4f3ea14674a090c3f4
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-09 20:15:54 +0200

    CreateContentActivity terminado

--- app/src/main/java/com/example/hector/proyectodamdaw/CreateContentActivity.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/CreateContentActivity.java
@@ -3,6 +3,11 @@ package com.example.hector.proyectodamdaw;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +20,9 @@ import android.view.MenuItem;
 
 public class CreateContentActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ViewPager viewPager;
+    private TabLayout tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,35 @@ public class CreateContentActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText(R.string.tab1));
+        tabs.addTab(tabs.newTab().setText(R.string.tab2));
+        tabs.addTab(tabs.newTab().setText(R.string.tab3));
+        tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+TabAdapterCreate adapter = new TabAdapterCreate(getSupportFragmentManager(), tabs.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
     }
 
     @Override
@@ -97,5 +134,43 @@ public class CreateContentActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    class TabAdapterCreate extends FragmentStatePagerAdapter {
+
+        //integer to count number of tabs
+        int tabCount;
+
+        //Constructor to the class
+        public TabAdapterCreate(FragmentManager fm, int tabCount) {
+            super(fm);
+            //Initializing tab count
+            this.tabCount= tabCount;
+        }
+
+        //Overriding method getItem
+        @Override
+        public Fragment getItem(int position) {
+            //Returning the current tabs
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment = new AllVotacionesFragment();
+                    break;
+                case 1:
+                    fragment = new AllPostFragment();
+                    break;
+                case 2:
+                    fragment = new AllProposalFragment();
+                    break;
+            }
+            return fragment;
+        }
+
+        //Overriden method getCount to get the number of tabs
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
