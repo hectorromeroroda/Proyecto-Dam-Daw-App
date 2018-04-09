9d8b61fc0e81f100614f46c38a9dce5c8ac8e8dc
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-09 15:58:51 +0200

    ProposalInProgressFrgment terminada

--- app/src/main/java/com/example/hector/proyectodamdaw/ProposalInProgressFragment.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/ProposalInProgressFragment.java
@@ -5,12 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 /**
  * Created by Hector on 09-Apr-18.
  */
 
 public class ProposalInProgressFragment extends Fragment{
+
+    TextView NameProposal;
+    TextView txvDescriptionProposal;
+    TextView txvQuestionProposal;
+    RadioButton rdbAcceptProposal;
+    RadioButton rdbDiscardProposal;
+    Button sendProposal;
 
 
     public ProposalInProgressFragment() {
@@ -22,7 +33,12 @@ public class ProposalInProgressFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.proposal_in_progress_fragment, container, false);
 
-
+        NameProposal =(TextView) view.findViewById(R.id.txvNameProposal);
+        txvDescriptionProposal =(TextView) view.findViewById(R.id.txvDescriptionProposal);
+        txvQuestionProposal =(TextView) view.findViewById(R.id.txvQuestionProposal);
+        sendProposal = (Button) view.findViewById(R.id.btnSendProposal);
+        rdbAcceptProposal=(RadioButton)view.findViewById(R.id.rdbAcceptProposal);
+        rdbDiscardProposal=(RadioButton)view.findViewById(R.id.rdbDiscardProposal);
 
         return view;
     }
