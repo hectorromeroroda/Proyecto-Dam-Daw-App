936082b7ed20690710104483d3058916bfaf953a
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-13 23:20:50 +0200

    Row Post terminada

--- app/src/main/java/com/example/hector/proyectodamdaw/RowPost.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/RowPost.java
@@ -3,11 +3,11 @@ package com.example.hector.proyectodamdaw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RowMyCommunitie extends AppCompatActivity {
+public class RowPost extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_row_my_communitie);
+        setContentView(R.layout.activity_row_post);
     }
 }
