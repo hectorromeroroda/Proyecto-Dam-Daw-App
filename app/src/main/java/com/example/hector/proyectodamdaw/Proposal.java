e123a943a4c570bb913b2e76a37c9e396d1b5d9b
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-16 22:45:34 +0200

    Algunas clases creadas, pero sin llenar
    clases votacion, propuesta etc. Falta llenarlas

---
+++ app/src/main/java/com/example/hector/proyectodamdaw/Proposal.java
@@ -0,0 +1,13 @@
+package com.example.hector.proyectodamdaw;
+
+/**
+ * Created by Hector on 16/04/2018.
+ */
+
+public class Proposal {
+
+    public Proposal() {
+       // this.NombreInmersion = NombreInmersion;
+
+    }
+}
