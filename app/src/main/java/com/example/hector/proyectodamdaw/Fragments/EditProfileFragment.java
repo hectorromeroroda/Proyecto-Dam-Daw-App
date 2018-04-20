11b90c27555f6db5b768d31dd9cddca8bd5763e0
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-04-20 16:28:20 +0200

    Modificaciones EditProfile y empezando EditProfile Fragment

---
+++ app/src/main/java/com/example/hector/proyectodamdaw/Fragments/EditProfileFragment.java
@@ -0,0 +1,38 @@
+package com.example.hector.proyectodamdaw.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hector.proyectodamdaw.R;
+
+/**
+ * Created by Hector on 20/04/2018.
+ */
+
+public class EditProfileFragment extends Fragment{
+
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.edit_profile_fragment, container, false);
+
+
+
+        return view;
+    }
+
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+
+
+    }
+
+}
