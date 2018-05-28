8de6b1a72b9b8ce236720288901f3c8612db7127
Hector Romero Roda <hectorromeroroda@gmail.com>
2018-05-29 01:55:04 +0200

    funciona ver post  poll y prop

--- app/src/main/java/com/example/hector/proyectodamdaw/Fragments/AllPostFragment.java
+++ app/src/main/java/com/example/hector/proyectodamdaw/Fragments/AllPostFragment.java
@@ -61,8 +61,6 @@ public class AllPostFragment extends Fragment{
         GlobalVariables globales = GlobalVariables.getInstance().getInstance();
         idComunidadActual=globales.getCommunityId();
         idUserSqlite=globales.getIdUserSqlite();
-
-        RefresPost();
         
         //ReciclerView de comunidades a las que pertenece
         adaptadorBd = new AdaptadorAllPostBD(getContext(),post,bd.todosPostCommunity(idComunidadActual));
@@ -80,6 +78,10 @@ public class AllPostFragment extends Fragment{
 
     }
 
+    public  void onStart(){
+        super.onStart();
+        RefresPost();
+    }
 
 
     private void RefresPost() {
